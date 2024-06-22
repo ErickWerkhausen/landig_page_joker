0510506c9dcbde6eb9d244d687082906d365e276
ErickWerkhausen <142852118+ErickWerkhausen@users.noreply.github.com>
2024-06-21 22:36:51 -0300

    alteracao

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const imagemin = require('gulp-imagemin');
 const uglify = require('gulp-uglify');
-const htmlmin = require('gulp-htmlmin');
+// const htmlmin = require('gulp-htmlmin');
 
 
 function styles() {
@@ -12,11 +12,11 @@ function styles() {
     
 }
 
-function minhtml() {
-    return gulp.src('./src/*.html')
-    .pipe(htmlmin({ collapseWhitespace: true }))
-    .pipe(gulp.dest('./dist'));
-}
+// function minhtml() {
+//     return gulp.src('./src/*.html')
+//     .pipe(htmlmin({ collapseWhitespace: true }))
+//     .pipe(gulp.dest('./dist'));
+// }
 
 function scripts() {
     
@@ -37,5 +37,5 @@ exports.default = buildImagemin;
 exports.watch = function() {
     gulp.watch('./src/styles/*.scss', gulp.parallel(styles))
     gulp.watch('./src/scripts/*.js', gulp.parallel(scripts))
-    gulp.watch('./src/*.html', gulp.parallel(minhtml))
+    // gulp.watch('./src/*.html', gulp.parallel(minhtml))
 }
